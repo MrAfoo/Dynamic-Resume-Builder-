e0ebeb1400fcfedee1cbd3f29a3ea0c67a4f6e64
Syed Affan Ali <160457228+MrAfoo@users.noreply.github.com>
2024-10-28 12:06:54 +0500

    first commit

---
+++ script.js
@@ -0,0 +1,7 @@
+"use strict";
+var contactInfo = document.querySelector('.contact');
+if (contactInfo) {
+    contactInfo.addEventListener('click', function () {
+        alert('Contact information clicked!');
+    });
+}
---
+++ script.ts
@@ -0,0 +1,8 @@
+
+const contactInfo = document.querySelector('.contact');
+
+if (contactInfo) {
+    contactInfo.addEventListener('click', () => {
+        alert('Contact information clicked!');
+    });
+}
